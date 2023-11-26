266b6c7ec04cd1ff7de935dfeef4e95f656f548c
HjalmarVaikuell <vaikuell.hjalmar@gmail.com>
2023-11-26 20:46:44 +0200

    Changed images to use internet links

--- src/components/PostsCompo.vue
+++ src/components/PostsCompo.vue
@@ -1,12 +1,13 @@
 <template>
 <div id="posts-list">
-<ul class="item" v-for="post in postsList" :key="post.id">
-<span class="profilePicture"></span>
-<img class="profile-picture" v-bind:src="'@/assets/' + post.user.profilePicture">
-<span class="user"> <b>Author: </b> {{ post.user.name }} </span>
-<span class="userName"> <b>Alias: </b> {{ post.user.userName }} </span>
-<span class="content"> <b>Content: </b> {{ post.content }} </span>
-<span class="date"> <b>Date: </b> {{ post.date }} </span>
+<ul class="post" v-for="post in postsList" :key="post.id">
+  <div class="post-header">
+    <img class="profile-picture" v-bind:src="post.user.profilePicture">
+    <span class="userName"> <b> </b> {{ post.user.userName }} </span>
+  </div>
+  <img class="content-picture" v-bind:src="post.picture">
+  <span class="content"> <b>Content: </b> {{ post.content }} </span>
+  <span class="date"> <b>Date: </b> {{ post.date }} </span>
 </ul>
 </div>
 </template>
@@ -20,15 +21,37 @@ export default {
 </script>
 
 <style scoped>
-.item {
-  background: rgb(128, 183, 235);
-  margin-bottom: 5px;
-  padding: 3px 5px;}
 #posts-list{
-  background: #1862c9;
+  display: flex;
+  flex-direction: column;
+  background: #acc8e1;
   box-shadow: 1px 2px 3px rgba(0,0,0,0.2);
   margin-bottom: 30px;
   padding: 10px 20px;}
+.post {
+  background: rgb(128, 183, 235);
+  width: 50dvw;
+  height: 60dvw;
+  align-self: center;
+  margin-bottom: 5px;
+  border-radius: 1cap;
+  padding: 3px 5px;}
+.post-header {
+  display: flex;
+  background-color: antiquewhite;
+  margin: 0.5dvw;}
+.profile-picture{
+  width: 7dvw;
+  height: 7dvw;
+}
+.userName {
+  align-self: center;
+  margin-left: 5px;
+}
+.content-picture {
+  width: 48dvw;
+  height: 48dvw;
+}
 /*#posts-list ul{
   padding: 0;
   list-style-type: none;
--- src/store/index.js
+++ src/store/index.js
@@ -9,9 +9,9 @@ export default createStore({
         user: {
           name: "Johnny Depp",
           userName: "JDepp123",
-          profilePicture: "/images/user1.jpg"
+          profilePicture: "https://img.freepik.com/premium-vector/cartoon-cute-cat-with-square-shape-square-icon-apps-games-vector-illustration-isolated_351178-23.jpg"
         },
-        picture: "/images/post1.jpg",
+        picture: "https://as2.ftcdn.net/v2/jpg/02/73/11/99/1000_F_273119914_9HDh3b4CBXZ8zSo4Ezyinadc5BUW3QI2.jpg",
         content: "I feel happy today!",
         date: "2023-09-27"
       },
@@ -20,9 +20,9 @@ export default createStore({
         user: {
           name: "Tom Cruise",
           userName: "Tommyboy",
-          profilePicture: "/images/user2.jpg"
+          profilePicture: "https://i.kym-cdn.com/photos/images/original/001/250/216/305.jpg"
         },
-        picture: "/images/post2.webp",
+        picture: "post2.webp",
         content: "Made a huge discovery today! Never going to mix a hen and a rooster up any more! #MoreEggs",
         date: "2023-09-24"
       },
@@ -31,9 +31,9 @@ export default createStore({
         user: {
           name: "Chris Hemsworth",
           userName: "ChrisHemsworthOfficial",
-          profilePicture: "/images/user3.jpg"
+          profilePicture: "https://thumbs.dreamstime.com/b/gray-fluffy-cat-blue-eyes-shot-close-up-square-image-173304539.jpg"
         },
-        picture: "/images/post3.jpeg",
+        picture: "post3.jpeg",
         content: "Sounds scary guys, Stay safe out there! #ImFeelingScared",
         date: "2023-09-17"
       },
@@ -42,7 +42,7 @@ export default createStore({
         user: {
           name: "Bruce Willis",
           userName: "MrDieHard",
-          profilePicture: "/images/user4.jpg"
+          profilePicture: "https://i.pinimg.com/originals/90/c0/34/90c034b9f17c0d1e93dc88d2f73859e7.jpg"
         },
         content: "I think I'll retire, I've had enough",
         date: "2022-03-04"
@@ -52,7 +52,7 @@ export default createStore({
         user: {
           name: "Scarlett Johansson",
           userName: "ScarlettJohansson84",
-          profilePicture: "/images/user1.jpg"
+          profilePicture: "https://static.vecteezy.com/system/resources/previews/006/873/332/non_2x/cat-emoji-square-emoticon-smile-cute-illustration-free-vector.jpg"
         },
         content: "Ate a 5 pound cake yesterday. Now feeling complete :D #CakeIsLife",
         date: "2022-02-21"
@@ -62,7 +62,7 @@ export default createStore({
         user: {
           name: "Kanye West",
           userName: "Yeezy",
-          profilePicture: "/images/user6.jpg"
+          profilePicture: "https://thumbs.dreamstime.com/b/tabby-cat-looking-out-jar-square-white-gray-old-stoneware-86472666.jpg"
         },
         content: "I'm nice at ping pong",
         date: "2022-01-25"
@@ -72,9 +72,9 @@ export default createStore({
         user: {
           name: "Lebron James",
           userName: "KingJames",
-          profilePicture: "/images/user7.jpg"
+          profilePicture: "https://images.ctfassets.net/sfnkq8lmu5d7/7Mq8f3j1ULBEZ1MALmvnog/a3ada063518e190c7acbd46142dcf7de/The_Wildest_Poets_Square_Cats_Snapinsta_1.jpg?w=750&h=750&fl=progressive&q=70&fm=jpg"
         },
-        picture: "/images/post7.jpg",
+        picture: "post7.jpg",
         content: "My love for music is INSANE!!! I think I'm going to do a album. #KidfromAkron #KingJames #GreatestOfAllTime #ChangedTheGame",
         date: "2021-12-03"
       },
@@ -83,7 +83,7 @@ export default createStore({
         user: {
           name: "Donald Trump",
           userName: "realDonaldTrump",
-          profilePicture: "/images/user8.jpg"
+          profilePicture: "https://static01.nyt.com/images/2021/07/07/world/asia/cat-billboard/cat-billboard-mediumSquareAt3X.jpg"
         },
         content: "Despite the constant negative press covfefe",
         date: "2021-09-30"
@@ -93,7 +93,7 @@ export default createStore({
         user: {
           name: "CIA",
           userName: "CIA",
-          profilePicture: "/images/user9.png"
+          profilePicture: "https://pbs.twimg.com/profile_images/1675159188475879424/Z7_mKZeY_400x400.jpg"
         },
         content: "We can neither confirm nor deny that this is our first tweet.",
         date: "2021-08-17"
@@ -103,9 +103,9 @@ export default createStore({
         user: {
           name: "Tom Cruise",
           userName: "Tommyboy",
-          profilePicture: "/images/user2.jpg"
+          profilePicture: "https://images.markonart.com/wp-content/uploads/2023/03/colorful-cat-stylized-mosaic-art-print.jpg.webp"
         },
-        picture: "/images/post10.jpg",
+        picture: "post10.jpg",
         content: "Remembering the time I met the wonderful Prince Philip #RIP",
         date: "2021-04-10"
       }
@@ -116,9 +116,10 @@ export default createStore({
       var postsListItem = state.storeList.map(post => {
             return {
                 id:post.id,
-                profilePicture:post.user.profilePicture,
+                profilePicturePath:post.user.profilePicture,
                 user:post.user.name,
                 userName:post.user.userName,
+                picture:post.picture,
                 content:post.content,
                 date:post.date
             }
